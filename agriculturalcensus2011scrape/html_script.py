fbe250977dad5f0114d787f813e69319b5b52f3d
Varun Goel <varungoel123@gmail.com>
2015-09-22 02:40:00 +0530

    working selenium navigation, comments added

--- agriculturalcensus2011scrape/html_script.py
+++ agriculturalcensus2011scrape/html_script.py
@@ -19,48 +19,60 @@ url = r'http://agcensus.dacnet.nic.in/StateSizeClass.aspx'
 driver.get(url) 
 driver.wait = WebDriverWait(driver, 2)
 
+tabUrlBody = "http://agcensus.dacnet.nic.in/sizestatedisplay" # url use later to navigate to other tables
 
-sizeclass_Select = Select(driver.find_element_by_id("_ctl0_ContentPlaceHolder1_DropDownList5")) 
+sizeclass_Select = Select(driver.find_element_by_id("_ctl0_ContentPlaceHolder1_DropDownList5")) # retrieve all sizeclasses
 
-sizeclass_Val = {}
+sizeclass_val = {}
 for val in sizeclass_Select.options:
-    sizeclass_Val[val.get_attribute('value')] = val.text
+    sizeclass_val[val.get_attribute('value')] = val.text # populate all sizeclasses
 
 
-for sizeclass in sizeclass_Val.keys():
+socialgroup_select = Select(driver.find_element_by_id("_ctl0_ContentPlaceHolder1_DropDownList4")) # social groups
+socialgroup_val = {}
+for val in socialgroup_select.options:
+    socialgroup_val[val.get_attribute('value')] = val.text # populate social groups
+
+url_append = ['3','2a','4','5a','6a','7'] # table numbers to be appended to tabUrlBody
+
+
+for sizeclass in sizeclass_val.keys(): # loop through each sizeclass
     driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList5"]/option[@value=' + sizeclass + ']').click()
     time.sleep(3) # wait for browser
-    
-    socialgroup_select = Select(driver.find_element_by_id("_ctl0_ContentPlaceHolder1_DropDownList4"))
-    socialgroup_val = {}
-    for val in socialgroup_select.options:
-        socialgroup_val[val.get_attribute('value')] = val.text
-    
 
-    for socialgroup in socialgroup_val.keys():
+    for socialgroup in socialgroup_val.keys(): # through each socialgroup
         driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList4"]/option[@value=' + socialgroup + ']').click()
         time.sleep(3)
-        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_Dropdownlist2"]//option[4]').click()
+        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_Dropdownlist2"]//option[4]').click() # select AP
         time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList1"]/option[2]').click()
+        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList1"]/option[2]').click() # select 2010-11
         time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList3"]/option[4]').click()
+        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList3"]/option[3]').click() # select landuse
         time.sleep(2)
-        driver.find_element_by_id("_ctl0_ContentPlaceHolder1_Submit").click()
-        time.sleep(4)
         
-        soup=BeautifulSoup(driver.page_source,"html.parser")
-        tag_body = soup.body
-        tag_table =tag_body.find_all("table")
-        tag_data = tag_table[3]
-        tag_tr = tag_data.find_all("tr")
-        data_all = pd.DataFrame()
-        samplist=[]
-        for i in range(3,len(tag_tr)): 
-            data = tag_tr[i].get_text(",", strip = True)
-            data_string = data.encode('utf-8')
-            data_list = data_string.split()
-            df = pd.DataFrame(data_list)
-            data_all = data_all.append(df)
-        #print data_all
-        data_all.to_csv('./sample_2.csv', header = False, index = False, sep='\t') 
+
+        for table in url_append: # navigate through each table and write
+            if table == '3':
+                driver.find_element_by_id("_ctl0_ContentPlaceHolder1_Submit").click() # click once only for landuse
+                time.sleep(4)
+                continue
+            driver.get(tabUrlBody + table + ".aspx")
+            time.sleep(4)
+            # soup=BeautifulSoup(driver.page_source,"html.parser")
+            # tag_body = soup.body
+            # tag_table =tag_body.find_all("table")
+            # tag_data = tag_table[3]
+            # tag_tr = tag_data.find_all("tr")
+            # data_all = pd.DataFrame()
+            # samplist=[]
+            # for i in range(3,len(tag_tr)): 
+            #     data = tag_tr[i].get_text(",", strip = True)
+            #     data_string = data.encode('utf-8')
+            #     data_list = data_string.split()
+            #     df = pd.DataFrame(data_list)
+            #     data_all = data_all.append(df)
+            # #print data_all
+            # data_all.to_csv('./sample_2.csv', header = False, index = False, sep='\t')
+
+        driver.execute_script("window.history.go(-6)")# navigate back to mainpage without losing selections
+        time.sleep(10)
