d1e9d43cca39955bfc7911b0f3a3594fd40443e3
Varun Goel <varungoel123@gmail.com>
2015-09-22 10:06:36 +0530

    corrected landuse scraping, and error not involving institutional

--- agriculturalcensus2011scrape/html_script.py
+++ agriculturalcensus2011scrape/html_script.py
@@ -21,18 +21,26 @@ driver.wait = WebDriverWait(driver, 2)
 
 tabUrlBody = "http://agcensus.dacnet.nic.in/sizestatedisplay" # url use later to navigate to other tables
 
+driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_Dropdownlist2"]//option[4]').click() # select AP
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList1"]/option[2]').click() # select 2010-11
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList3"]/option[3]').click() # select landuse
+time.sleep(2)
+
 sizeclass_Select = Select(driver.find_element_by_id("_ctl0_ContentPlaceHolder1_DropDownList5")) # retrieve all sizeclasses
 
 sizeclass_val = {}
 for val in sizeclass_Select.options:
     sizeclass_val[val.get_attribute('value')] = val.text # populate all sizeclasses
-
+print sizeclass_val
 
 socialgroup_select = Select(driver.find_element_by_id("_ctl0_ContentPlaceHolder1_DropDownList4")) # social groups
 socialgroup_val = {}
 for val in socialgroup_select.options:
     socialgroup_val[val.get_attribute('value')] = val.text # populate social groups
 
+print socialgroup_val
 url_append = ['3','2a','4','5a','6a','7'] # table numbers to be appended to tabUrlBody
 #url_samp = ['3','2a']
 
@@ -43,19 +51,14 @@ for sizeclass in sizeclass_val.keys(): # loop through each sizeclass
     for socialgroup in socialgroup_val.keys(): # through each socialgroup
         driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList4"]/option[@value=' + socialgroup + ']').click()
         time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_Dropdownlist2"]//option[4]').click() # select AP
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList1"]/option[2]').click() # select 2010-11
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="_ctl0_ContentPlaceHolder1_DropDownList3"]/option[3]').click() # select landuse
-        time.sleep(2)
+
         fixed_val = sizeclass_val[sizeclass] + ',' + socialgroup_val[socialgroup] + ',' # add sizegroup and social class column values
 
         for table in url_append: # navigate through each table and write
             if table == '3':
                 driver.find_element_by_id("_ctl0_ContentPlaceHolder1_Submit").click() # click once only for landuse
                 time.sleep(3)
-                continue
+                
             driver.get(tabUrlBody + table + ".aspx")
             time.sleep(3)
             soup=BeautifulSoup(driver.page_source,"html.parser")
